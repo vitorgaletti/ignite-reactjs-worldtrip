df668e9417efcbc4223c013091e454a1836123e7
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-02-23 21:58:32 -0300

    :bento: Add airplane and banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Flex } from '@chakra-ui/react';
+import { Banner } from '../components/Banner';
 import { Header } from '../components/Header';
 
 export default function Home() {
   return (
     <Flex direction="column" w="100%" h="100vh">
       <Header />
+      <Banner />
     </Flex>
   );
 }

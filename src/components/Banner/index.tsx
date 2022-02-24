bbf648ca9404e348fbdf7c5c8ac4a9d2be4b6ea1
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-02-24 11:13:44 -0300

    :iphone: Add responsive banner component

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,46 +1,52 @@
-import { Box, Flex, Heading, Image, SimpleGrid, Text } from '@chakra-ui/react';
+import { Box, Flex, Heading, Image, Text } from '@chakra-ui/react';
 
 export function Banner() {
   return (
-    <SimpleGrid
-      columns={2}
-      spacing="205.25px"
+    <Flex
       w="100%"
-      h="335px"
+      h={['163px', '335px']}
       bgImage="url(/banner.png)"
       bgSize="cover"
       bgRepeat="no-repeat"
-      px="8.75rem"
+      px={['1rem', '8.75rem']}
+      gap={['205.25px']}
     >
-      <Box mx="auto" my="5rem">
-        <Heading
-          fontWeight="500"
-          fontSize="4xl"
-          lineHeight="54px"
-          color="gray.50"
-        >
-          5 Continentes, <br /> infinitas possibilidades.
-        </Heading>
+      <Flex flexDirection="column" mx="auto" py={['1.75rem', '5rem']}>
+        <Box w="100%" maxWidth={['238px', '426px']}>
+          <Heading
+            fontWeight="500"
+            fontSize={['xl', '3xl', '4xl']}
+            lineHeight={['30px', '46px', '46px', '42px', '54px']}
+            color="gray.50"
+          >
+            5 Continentes, infinitas possibilidades.
+          </Heading>
+        </Box>
 
-        <Text
-          fontWeight="normal"
-          fontSize="xl"
-          lineHeight="30px"
-          color="gray.200"
-          mt="1.25rem"
-        >
-          Chegou a hora de tirar do papel a viagem que <br />
-          você sempre sonhou.
-        </Text>
-      </Box>
-      <Box maxWidth="417.15px" mt="4.75rem">
+        <Box w="100%" maxWidth={['333px', '524px']} mt={['0.5rem', '1.25rem']}>
+          <Text
+            fontWeight="normal"
+            fontSize={['sm', 'md', 'xl']}
+            lineHeight={['21px', '24px', '30px']}
+            color="gray.200"
+          >
+            Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+          </Text>
+        </Box>
+      </Flex>
+
+      <Box
+        maxWidth="417.15px"
+        mt="4.75rem"
+        display={['none', 'none', 'none', 'block']}
+      >
         <Image
           src="./airplane.svg"
           alt="Avião"
           w="100%"
           transform="rotate(3deg)"
-        ></Image>
+        />
       </Box>
-    </SimpleGrid>
+    </Flex>
   );
 }

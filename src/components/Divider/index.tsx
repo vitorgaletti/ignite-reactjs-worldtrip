e5d41e0945a030cf98cdf8d12b6ed04765c74d17
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-02-25 10:57:29 -0300

    :recycle: Modify divider component

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -2,25 +2,13 @@ import { Flex, Heading } from '@chakra-ui/react';
 
 export function Divider() {
   return (
-    <>
-      <Flex
-        align="center"
-        justify="center"
-        mx="auto"
-        w={['3.75rem', '5.625rem']}
-        border={['1px solid #47585B', '2px solid #47585B']}
-        mt={['2.25rem', '5rem']}
-      ></Flex>
-      <Heading
-        fontWeight="500"
-        fontSize={['xl', '4xl']}
-        lineHeight={['30px', '54px']}
-        textAlign="center"
-        color="gray.700"
-        mt={['1.5rem', '3.25rem']}
-      >
-        Vamos nessa? <br /> Então escolha seu continente
-      </Heading>
-    </>
+    <Flex
+      align="center"
+      justify="center"
+      mx="auto"
+      w={['3.75rem', '5.625rem']}
+      border={['1px solid #47585B', '2px solid #47585B']}
+      mt={['2.25rem', '5rem']}
+    ></Flex>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Heading } from '@chakra-ui/react';
 import { Banner } from '../components/Banner';
 import { Divider } from '../components/Divider';
 import { Header } from '../components/Header';
@@ -11,6 +11,16 @@ export default function Home() {
       <Banner />
       <TravelTypes />
       <Divider />
+      <Heading
+        fontWeight="500"
+        fontSize={['xl', '4xl']}
+        lineHeight={['30px', '54px']}
+        textAlign="center"
+        color="gray.700"
+        mt={['1.5rem', '3.25rem']}
+      >
+        Vamos nessa? <br /> Então escolha seu continente
+      </Heading>
     </Flex>
   );
 }

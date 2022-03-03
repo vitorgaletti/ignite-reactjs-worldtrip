4c19bd22a23aa7c4a60b20d9ccd92af2465e1ed0
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-03-03 15:31:08 -0300

    :lipstick: Add arrow left icon in Header Component

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,13 +1,20 @@
 import { Image } from '@chakra-ui/react';
 
-export function Logo() {
+interface LogoProps {
+  justifySelf: React.CSSProperties | any;
+  gridColumn: React.CSSProperties | any;
+}
+
+export function Logo({ gridColumn, justifySelf }: LogoProps) {
   return (
     <Image
       w="100%"
       maxWidth={['81px', '184px']}
       h={['20px', '45.92px']}
-      src="./logo.svg"
+      src="/logo.svg"
       alt="Logo worldtrip"
+      gridColumn={gridColumn}
+      justifySelf={justifySelf}
     ></Image>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,13 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex, Grid, Icon } from '@chakra-ui/react';
+import { RiArrowLeftSLine } from 'react-icons/ri';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { Logo } from './Logo';
 
 export function Header() {
+  const { asPath } = useRouter();
+  const notHomePage = asPath !== '/';
+
   return (
     <Flex
       as="header"
@@ -13,9 +19,30 @@ export function Header() {
       justify="center"
       bg="white"
     >
-      <Flex align="center" justify="space-between">
-        <Logo />
-      </Flex>
+      <Grid
+        w="100%"
+        h="100%"
+        mx="auto"
+        templateColumns="repeat(3, 1fr)"
+        maxWidth="1160px"
+        alignItems="center"
+        justifyContent="center"
+      >
+        {notHomePage && (
+          <Link href="/">
+            <a>
+              <Icon
+                as={RiArrowLeftSLine}
+                fontSize={[20, 40]}
+                justifySelf="start"
+              >
+                Olá
+              </Icon>
+            </a>
+          </Link>
+        )}
+        <Logo gridColumn="2" justifySelf="center" />
+      </Grid>
     </Flex>
   );
 }
